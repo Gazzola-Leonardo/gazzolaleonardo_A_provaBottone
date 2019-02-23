83b06f4574d1000df89674b7cb0a9fe23e030bfa
Gazzola Leonardo <leonardo.gazzola@barsanti.edu.it>
2019-02-23 11:14:40 +0100

    Scrittura codice

--- gazzolaleonardo_A_provaBottone.ino
+++ gazzolaleonardo_A_provaBottone.ino
@@ -1,8 +1,23 @@
+#define LED 13
+#define BOTTON 7
+int val = 0;
+
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED, OUTPUT);
+  pinMode(BOTTON, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  val = digitalRead(BUTTON);
+
+  if(val == HIGH){
+    digitalWrite(LED, HIGH);
+  }
+  else{
+    digitalWrite(LED, LOW);
+  }  
 }
